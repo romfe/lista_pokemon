c9b85fc1c6439497075be50a18138d51b41fe917
Rômulo <romfe89@gmail.com>
2021-08-13 14:11:43 -0300

    adicionados os componentes do filtro

--- src/components/Filtro/FiltroCP.tsx
+++ src/components/Filtro/FiltroCP.tsx
@@ -1,10 +1,45 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './FiltroCP.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
+
+function valuetext(value: any) {
+  return `${value}°C`;
+}
+
+
+
+
+
 const FiltroCP: FC = () => {
+  const classes = useStyles();
+  const [value, setValue] = useState([20, 37]);
+
+  const handleChange = (event: any, newValue: any) => {
+    setValue(newValue);
+  };
   return (
     <div>
-      <p>inserir trackbar</p>
+      <Typography id="range-slider" gutterBottom>
+        Temperature range
+      </Typography>
+      <div className={classes.root}>
+        <Slider
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          getAriaValueText={valuetext}
+        />
+      </div>
     </div>
   );
 };
--- src/components/Filtro/ListaTipos.tsx
+++ src/components/Filtro/ListaTipos.tsx
@@ -1,10 +1,43 @@
 import { FC } from 'react';
 import './ListaTipos.css';
 
+import {
+  Checkbox,
+  CheckboxGroup,
+  ChakraProvider
+} from "@chakra-ui/react"
+
 const ListaTipos: FC = () => {
-  const tipos: string[] = ['Normal', 'Water', 'Poison'];
+  const tipos: string[] = [
+    'Normal',
+    'Water',
+    'Poison',
+    'Psychic',
+    'Bug',
+    'Dark',
+    'Fire',
+    'Flying',
+    'Electric',
+    'Rock',
+    'Dragon',
+    'Steel',
+    'Fighting',
+    'Grass',
+    'Ground',
+    'Ice',
+    'Ghost',
+    'Fairy'
+  ];
   return (
     <div>
+      <ChakraProvider>
+        {tipos.map((tipo: string) => <Checkbox
+          size="md"
+          colorScheme="blue"
+          defaultIsChecked
+        >{tipo}</Checkbox>
+        )}
+      </ChakraProvider>
 
     </div>
   );
